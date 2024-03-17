48f4cf7140840561c438069764bc9ad0019359e7
minyoung <alsduddl9977@gmail.com>
2024-03-17 17:55:16 +0900

    단위테스트

--- src/pages/cart/components/tests/EmptyNotice.spec.jsx
+++ src/pages/cart/components/tests/EmptyNotice.spec.jsx
@@ -4,4 +4,21 @@ import React from 'react';
 import EmptyNotice from '@/pages/cart/components/EmptyNotice';
 import render from '@/utils/test/render';
 
-it('"홈으로 가기" 링크를 클릭할경우 "/"경로로 navigate함수가 호출된다', async () => {});
+const navigateFn = vi.fn();
+
+vi.mock('react-router-dom', async () => {
+  const original = await vi.importActual('react-router-dom');
+
+  return { ...original, useNavigate: () => navigateFn };
+});
+
+it('"홈으로 가기" 링크를 클릭할경우 "/"경로로 navigate함수가 호출된다', async () => {
+  const { user } = await render(<EmptyNotice />);
+
+  const link = screen.getByText('홈으로 가기');
+  console.log(link, 'link');
+
+  await user.click(link);
+
+  expect(navigateFn).toHaveBeenNthCalledWith(1, '/');
+});
